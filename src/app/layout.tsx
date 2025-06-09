3458b58e0448641b84496f05ec15fa4254f8052d
MadJangE00 <jiniixz@naver.com>
2025-06-09 09:42:41 +0900

    gestilt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Tiltburger | Indieverse",
+  title: "GesTiltBugger | Indieverse",
   description: "Indieverse의 소셜 미디어 플랫폼",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Tiltburger</h1>
+        <h1 className="text-4xl font-bold">GesTilt Bugger</h1>
         <p className="text-sm text-gray-500">copyright 2025. Indieverse. All rights reserved.</p>
         
       </main>
