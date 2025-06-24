933306b51da4520bb82ce1987391f15981f8cde0
MadJangE00 <jiniixz@naver.com>
2025-06-24 10:31:52 +0900

    redirection edit

--- src/app/auth/login/google/page.tsx
+++ src/app/auth/login/google/page.tsx
@@ -6,7 +6,9 @@ import { signIn } from 'next-auth/react';
 
 export default function GoogleLoginPage() {
   useEffect(() => {
-    signIn('google');
+    signIn('google', {
+      callbackUrl: 'https://nextwords-six.vercel.app/api/auth/google/callback'
+    });
   }, []);
 
   return <p>구글 로그인 중입니다...</p>;
